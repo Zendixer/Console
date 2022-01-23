c6e611a00aab267106650c3a8ee4f9e13f813962
Zendixer <98160592+Zendixer@users.noreply.github.com>
2022-01-23 22:40:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(My_first_console_project);
+var firstname = "The";
+var lastname="Rock"
+firstname.concat(lastname);
+var a="10";
+var b="5";
+var add= a+b;
+console.log(add);
+var subtract= a-b;
+console.log(subtract);
+var multiply= a*b;
+console.log(multiply);
+var divide= a/b;
+console.log(divide);
